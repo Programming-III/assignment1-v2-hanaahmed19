23c471a93f110febf5000d3c9bea105e195d0a3a
hanaahmed19 <hhana.ahmeddd77@gmail.com>
2025-11-11 09:55:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,19 @@ using namespace std;
 
 // ==================== Person Class Implementation =========================
 
+class person{
+    
+    
+    
+void person:: class display(){
 
+cout<<"name:"<<name<<endl;
+cout<<"id:"<<id<<endl;
+
+
+
+
+}
 
 
 
@@ -19,7 +31,15 @@ using namespace std;
 
 // ==================== Student Class Implementation ====================
 
+void student::class display(){
 
+
+person.display();
+cout<<"year:"<<yearLevel<<endl;
+cout<<"major:"<<major<<endl;
+
+
+}
 
 
 
@@ -27,12 +47,33 @@ using namespace std;
 // ==================== Instructor Class Implementation ====================
 
 
+void instructor::class display(){
+person.display();
+student.display();
+cout<<"department:"<<department<<endl;
+cout<<"experience:"<<experienceYears<<endl;
 
+
+
+
+}
 
 
 
 // ==================== Course Class Implementation ====================
-
+class course:class student{
+    
+    
+    
+ void course::class displayCourseInfo(){
+student.display();
+ 
+ cout<<"courseCode:"<<courseCode<<endl;
+cout<<"courseName:"<<courseName<<endl;
+  cout<<" maxStudents:"<< maxStudents<<endl;
+cout<<"currentStudents:"<<currentStudents<<endl;
+ 
+ }
 
 
 
@@ -42,6 +83,24 @@ using namespace std;
 
 // ==================== Main Function ====================
 int main() {
+   cout<<"course:CS101"<<endl;
+    student s1=new student();
+    
+    instructor i1= new instructor();
+    
+    course c1=new course();
+    
+    
+cin>>name;
+cin>>id;
+cin>>yearLevel;
+cin>> major;
+cin>>department;
+cin>>experienceYears;
+cin>>courseCode;
+cin>>courseName;
+cin>>maxStudents;
+cin>>currentStudents;
    
     
     return 0;
